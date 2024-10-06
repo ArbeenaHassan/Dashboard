82f394afabd52a1a13f8944391a7cdb4d15b35a3
ESHAN IQBAL <110513353+Eshaniqbal@users.noreply.github.com>
2024-10-06 11:06:03 +0530

    added

---
+++ src/screens/FigmaDesign/FigmaDesign.jsx
@@ -0,0 +1,230 @@
+import React from "react";
+import "./style.css";
+
+export const FigmaDesign = () => {
+  return (
+    <div className="figma-design">
+      <div className="root-wrapper">
+        <div className="root">
+          <div className="overlap">
+            <div className="overlap-group">
+              <div className="groups">
+                <div className="overlap-wrapper">
+                  <div className="div">
+                    <div className="groups-2">
+                      <img className="image" alt="Image" src="/img/image.png" />
+                      <img className="background" alt="Background" src="/img/background-1.png" />
+                      <div className="text-wrapper">79%</div>
+                      <img className="img" alt="Image" src="/img/image-1.png" />
+                      <div className="text-wrapper-2">Philosophy</div>
+                    </div>
+                    <div className="groups-3">
+                      <img className="image-2" alt="Image" src="/img/image-2.png" />
+                      <img className="background-2" alt="Background" src="/img/background-2.png" />
+                      <div className="text-wrapper-3">56%</div>
+                      <img className="image-3" alt="Image" src="/img/image-3.png" />
+                      <div className="text-wrapper-4">Meditation</div>
+                    </div>
+                    <div className="groups-4">
+                      <img className="image-4" alt="Image" src="/img/image-4.png" />
+                      <img className="background-3" alt="Background" src="/img/background-3.png" />
+                      <div className="text-wrapper-5">33%</div>
+                      <img className="image-5" alt="Image" src="/img/image-5.png" />
+                      <div className="text-wrapper-6">Leadership</div>
+                    </div>
+                    <div className="groups-5">
+                      <img className="image-6" alt="Image" src="/img/image-6.png" />
+                      <img className="background-4" alt="Background" src="/img/background-4.png" />
+                      <div className="text-wrapper-7">92%</div>
+                      <img className="image-7" alt="Image" src="/img/image-7.png" />
+                      <div className="text-wrapper-8">Blogging</div>
+                    </div>
+                    <div className="groups-6">
+                      <div className="overlap-group-2">
+                        <img className="image-8" alt="Image" src="/img/image-8.png" />
+                        <img className="image-9" alt="Image" src="/img/image-9.png" />
+                      </div>
+                      <img className="background-5" alt="Background" src="/img/background-5.png" />
+                      <div className="text-wrapper-9">71%</div>
+                      <img className="image-10" alt="Image" src="/img/image-10.png" />
+                      <div className="text-wrapper-10">Sport Skills</div>
+                    </div>
+                    <div className="groups-7">
+                      <p className="most-common-area">Most common areas of interests</p>
+                      <div className="text-wrapper-11">Developed areas</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="overlap-2">
+                  <div className="groups-8">
+                    <img className="image-11" alt="Image" src="/img/image-11.png" />
+                    <div className="text-wrapper-12">See all meetings</div>
+                    <div className="text-wrapper-13">Zoom</div>
+                    <img className="image-12" alt="Image" src="/img/image-12.png" />
+                    <div className="text-wrapper-14">04:00 pm</div>
+                    <div className="text-wrapper-15">7</div>
+                    <div className="lead-designers-e">Lead Designers Event</div>
+                    <div className="text-wrapper-16">Tue, 15 Jul</div>
+                  </div>
+                  <img className="background-6" alt="Background" src="/img/background-6.png" />
+                  <div className="groups-9">
+                    <div className="text-wrapper-17">Google Meet</div>
+                    <img className="image-13" alt="Image" src="/img/image-13.png" />
+                    <div className="text-wrapper-18">7</div>
+                    <p className="call-with-a-new">Call With a New Team</p>
+                    <div className="groups-10">
+                      <div className="text-wrapper-19">02:30 pm</div>
+                      <div className="text-wrapper-20">Tue,12 Jul</div>
+                    </div>
+                  </div>
+                  <img className="background-7" alt="Background" src="/img/background-7.png" />
+                  <div className="groups-11">
+                    <div className="text-wrapper-21">Google Meet</div>
+                    <img className="image-14" alt="Image" src="/img/image-14.png" />
+                    <img className="image-15" alt="Image" src="/img/image-15.png" />
+                    <div className="text-wrapper-22">John Onboarding</div>
+                    <div className="groups-10">
+                      <div className="text-wrapper-23">09:30 pm</div>
+                      <div className="text-wrapper-24">Tue.11Jul</div>
+                    </div>
+                  </div>
+                  <img className="background-8" alt="Background" src="/img/background-8.png" />
+                  <div className="groups-12">
+                    <div className="text-wrapper-25">Zoom</div>
+                    <img className="image-16" alt="Image" src="/img/image-16.png" />
+                    <div className="text-wrapper-26">08:15 am</div>
+                    <img className="image-17" alt="Image" src="/img/image-17.png" />
+                    <div className="quick-daily-meet">Quick Daily Meeting</div>
+                    <div className="text-wrapper-27">Tue, 11 Jul</div>
+                  </div>
+                </div>
+                <div className="groups-13">
+                  <img className="image-18" alt="Image" src="/img/image-18.png" />
+                  <div className="text-wrapper-28">My meetings</div>
+                </div>
+              </div>
+              <img className="background-9" alt="Background" src="/img/background-9.png" />
+              <div className="groups-14">
+                <div className="text-wrapper-29">Avg. Conc-ion</div>
+                <p className="min-or-lack-of-f">Min or lack of focus</p>
+                <img className="background-10" alt="Background" src="/img/background-10.png" />
+                <div className="text-wrapper-30">Maximum of focus</div>
+                <img className="background-11" alt="Background" src="/img/background-11.png" />
+                <img className="image-19" alt="Image" src="/img/image-19.png" />
+                <div className="text-wrapper-31">41%</div>
+                <div className="text-wrapper-32">Nov</div>
+                <div className="text-wrapper-33">Oct</div>
+                <button className="button">
+                  <div className="div-wrapper">
+                    <div className="text-wrapper-34">Sep</div>
+                  </div>
+                </button>
+                <div className="text-wrapper-35">Aug</div>
+                <div className="overlap-3">
+                  <div className="overlap-4">
+                    <button className="overlap-group-wrapper">
+                      <div className="overlap-5">
+                        <img className="image-20" alt="Image" src="/img/image-20.png" />
+                        <div className="range-last-mon">Range:&nbsp;&nbsp;Last month</div>
+                      </div>
+                    </button>
+                    <div className="overlap-6">
+                      <img className="image-21" alt="Image" src="/img/image-21.png" />
+                      <div className="button-2">
+                        <div className="overlap-7">
+                          <div className="text-wrapper-36">Unbalanced</div>
+                          <div className="text-wrapper-37">Week 8</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="productivity-ana">Productivity analytics</div>
+                  <div className="text-wrapper-38">Focusing</div>
+                </div>
+                <img className="image-22" alt="Image" src="/img/image-22.png" />
+              </div>
+              <div className="groups-15">
+                <img className="image-23" alt="Image" src="/img/image-36.png" />
+                <div className="text">
+                  <div className="overlap-group-3">
+                    <div className="text-wrapper-39">Search</div>
+                    <img className="image-24" alt="Image" src="/img/image-37.png" />
+                  </div>
+                </div>
+                <div className="your-personal-da">Your personal dashboard overview</div>
+                <div className="text-wrapper-40">Welcome, Kristin</div>
+                <img className="image-25" alt="Image" src="/img/image-38.png" />
+              </div>
+            </div>
+            <div className="groups-16">
+              <div className="overlap-8">
+                <div className="element-active-connect">3 active connections</div>
+                <img className="image-26" alt="Image" src="/img/image-23.png" />
+                <div className="overlap-group-4">
+                  <img className="image-27" alt="Image" src="/img/image-24.png" />
+                  <img className="image-28" alt="Image" src="/img/image-25.png" />
+                  <div className="image-29" />
+                  <img className="image-30" alt="Image" src="/img/image-26.png" />
+                </div>
+                <div className="trackers-connect">Trackers connected</div>
+              </div>
+            </div>
+            <div className="groups-17">
+              <div className="overlap-9">
+                <div className="text-wrapper-41">Avg. Completed</div>
+                <div className="text-wrapper-42">56%</div>
+                <img className="image-31" alt="Image" src="/img/image-28.png" />
+                <div className="additional-tasks">
+                  Additional
+                  <br />
+                  tasks
+                </div>
+              </div>
+            </div>
+            <div className="groups-18">
+              <div className="overlap-10">
+                <div className="text-wrapper-43">Avg. Completed</div>
+                <div className="text-wrapper-44">83%</div>
+                <img className="image-32" alt="Image" src="/img/image-30.png" />
+                <div className="prioritized-task">
+                  Prioritized
+                  <br />
+                  tasks
+                </div>
+              </div>
+            </div>
+            <div className="groups-19">
+              <div className="overlap-11">
+                <button className="button-3">
+                  <div className="overlap-group-5">
+                    <div className="text-wrapper-45">12</div>
+                    <img className="image-33" alt="Image" src="/img/image-31.png" />
+                  </div>
+                </button>
+                <button className="button-4">
+                  <div className="overlap-12">
+                    <div className="text-wrapper-46">56</div>
+                    <img className="image-33" alt="Image" src="/img/image-32.png" />
+                  </div>
+                </button>
+                <button className="button-5">
+                  <div className="overlap-13">
+                    <div className="text-wrapper-47">11</div>
+                    <img className="image-34" alt="Image" src="/img/image-33.png" />
+                  </div>
+                </button>
+                <div className="text-wrapper-48">Design Manager</div>
+                <div className="text-wrapper-49">Kristin Watson</div>
+                <div className="overlap-14">
+                  <div className="text-wrapper-50">0</div>
+                </div>
+                <img className="image-35" alt="Image" src="/img/image-35.png" />
+                <div className="text-wrapper-51">Profile</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
